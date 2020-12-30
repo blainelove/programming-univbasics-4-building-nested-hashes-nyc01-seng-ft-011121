760ef08de6f96d71641d369092dcc395f9276f6d
Blaine R Love <blainelove94@gmail.com>
2020-12-30 20:57:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,8 @@ end
 def monopoly_with_second_tier
   {
     :railroads => {
-      :pices => 4
-      :rent_in_dollars =>{}
+      :pices => 4,
+      :rent_in_dollars =>{},
       :names => {}
     }
   }
